ba745385891d565565eff8475a6d8ad1de76c8e3
Vijay <vjvijay130@gmail.com>
2023-07-06 17:37:36 +0530

    testing databinding

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -9,9 +9,25 @@ export class TestingComponent {
 
   serverId : number = 10;
   serveStatus : string = "Offline";
+  allownewserver = false;
+  serverName = "";
+
+  constructor(){
+    setTimeout(()=> {
+      this.allownewserver = true;
+    },2000);
+  }
 
   getserverstatus(){
     return this.serveStatus;
   }
 
+  oncreateserver(){
+    this.serveStatus = "server was created!";
+  }
+
+  onupdateserver(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
 }
