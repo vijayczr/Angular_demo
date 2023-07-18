db9d39e26fe355a6202c47707c591203ec6436d6
Vijay <vjvijay130@gmail.com>
2023-07-18 18:15:48 +0530

    new recepies using model and displaing recepe details

--- src/app/recipies/recipies-list/recipies-list.component.ts
+++ src/app/recipies/recipies-list/recipies-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from './recipe.model';
 })
 export class RecipiesListComponent implements OnInit{
  recipes : Recipe[] = [
-        new Recipe('A test Recepe','This is simply a test','https://www.howtocook.recipes/homemade-ratatouille-recipe/')
+        new Recipe('A test Recepe','This is simply a test',
+                    'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80'),
+        new Recipe('A test Recepe','This is simply a test',
+                    'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80')            
  ];
 
   ngOnInit(): void {
