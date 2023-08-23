9a05f97a8b3229dcc9c3aeb9e55030e387126a75
unknown <vjvijay130@gmail.com>
2023-08-23 12:34:05 +0530

    nav bar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_demo';
+  loadedFeature = "recepe";
+  onNavigate(feature:string)
+  {
+    this.loadedFeature = feature;
+  }
 }
